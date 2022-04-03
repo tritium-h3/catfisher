5f8ed479672c764436508dd36bd61505e60ea95f
Arielle F <bb8andr2d2forlife@gmail.com>
2022-04-03 13:02:05 -0400

    Added tilemap

---
+++ multiarielle/assets/Outdoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Outdoor" tilewidth="32" tileheight="32" tilecount="350" columns="10">
+ <image source="Outdoor.png" width="320" height="1120"/>
+</tileset>
